eeea1e47e6ab790d66547c758ae42b7495d43444
SpudlyHubber <rhysurquhart1@gmail.com>
2025-07-13 12:34:53 +0100

    Update project.js

--- project.js
+++ project.js
@@ -1,4 +1,11 @@
 let w = window.open("about:blank");
+self.addEventListener('message', function (e) {
+    if (e.data === 'run the timer') {
+        setInterval(function() {
+            self.postMessage('timer tick');
+        }, 1000);
+    }
+});
 function loop() {
     w.location.reload();
     requestAnimationFrame(loop);
