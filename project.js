01fba0b95d8e96ddba282fa6a6fc38fab33c0144
SpudlyHubber <rhysurquhart1@gmail.com>
2025-07-14 09:07:17 +0100

    Update project.js

--- project.js
+++ project.js
@@ -1,6 +1,14 @@
 let w = open("about:blank");
 function loop() {
     w.location.reload();
-    requestAnimationFrame(loop);
+    SetInterval(() => {
+        try {
+            w.location.reload();
+        } 
+        catch (e) {
+            console.warn("Error :(\n"+e); // most browsers have some thing for anti spam .reload()
+        }
+        loop();
+    }, 100); // this is in ms
 }
 loop();
