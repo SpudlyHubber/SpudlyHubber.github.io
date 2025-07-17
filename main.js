eeb917838f5defa95587f362657d3aa93acf5ffb
SpudlyHubber <rhysurquhart1@gmail.com>
2025-07-17 15:36:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function process() {
+    try {
+        let n = parseInt(text.value);
+        for(var i = 0; i < n; i++) {
+            open("project.html");
+        }
+    }
+    catch {
+    }
+}
